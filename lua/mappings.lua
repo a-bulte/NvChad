aa189b1147ba759fc52e4e8d2fda3d49388ff564
bulte <alexandre.bulte@proton.me>
2024-10-06 12:48:34 +0200

    feat: Mapping to open terminal with lazygit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,4 +10,12 @@ map("i", "jk", "<ESC>")
 -- nvim-tree
 map("n", "<leader>e", "<cmd> NvimTreeToggle <cr>", { desc = "Toggle NvimTree" })
 
+-- lazygit
+map({ "n", "t" }, "<C-g>", function ()
+  require("nvchad.term").toggle
+  { pos = "float",
+    id = "lazygit_term",
+    cmd = "lazygit" }
+end, { desc = "Open floating term with lazygit" } )
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
