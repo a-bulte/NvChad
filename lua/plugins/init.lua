6e4639abdf02cb22b03e005d4a97c63ada14cceb
bulte <alexandre.bulte@proton.me>
2024-10-06 14:11:11 +0200

    feat: Plugin to highlight todo comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -40,6 +40,15 @@ return {
     event = "VeryLazy",
   },
 
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      require("todo-comments").setup({})
+    end,
+    event = "VeryLazy",
+  }
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
